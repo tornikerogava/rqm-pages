ecaa1b99e433200ed6541b7564bc44803a30440d
tornikerogava <tokorogava@gmail.com>
2021-06-09 19:00:56 +0400

    First working variant
    No styling yet, but works basically as it should

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import {FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton,} from "react-share";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button, Dropdown, DropdownButton} from 'react-bootstrap'
 
-function App() {
+export default function App() {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+  const [topic, setTopic] = useState("");
+
+  const famousApi = topic == ""? 'http://api.quotable.io/random?tags=famous-quotes' : `http://api.quotable.io/random?tags=famous-quotes,${topic}`;
+  const topicTitle = topic == ""? `All topics` : topic.charAt(0).toUpperCase() + topic.slice(1);
+  
+  const newQuote=()=>{
+    fetch(`http://api.quotable.io/random?tags=${topic}`)
+    .then(quote => quote.json())
+    .then(quote=>{
+      setText(quote.content);
+      setAuthor(quote.author)
+      }
+    )
+  }
+  const famousQuote=()=>{
+    fetch(famousApi)
+    .then(quote => quote.json())
+    .then(quote=>{
+      setText(quote.content);
+      setAuthor(quote.author)
+      }
+    )
+  }
+  
+ 
+
+  useEffect(newQuote, [])
+  
+  const fullQuote = `"${text}" - ${author}`
+  const quoteWithHash = `"${text}" - ${author} #QuoteMachine`
+  const url = "github.com"
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div id="quote-box">
+        <div id="text">
+          {text}
+        </div>
+        <div id="author">
+          {author}
+        </div>
+        <DropdownButton id="topic-dropdown" variant="secondary" title={topicTitle}>
+        <Dropdown.Item onClick={()=> setTopic("")}>All Topics</Dropdown.Item>
+        <Dropdown.Item onClick={()=> setTopic("wisdom")}>Wisdom</Dropdown.Item>
+        <Dropdown.Item onClick={()=> setTopic("inspirational")}>Inspirational</Dropdown.Item>
+        <Dropdown.Item onClick={()=> setTopic("technology")}>Technology</Dropdown.Item>
+        <Dropdown.Item onClick={()=> setTopic("friendship")}>Friendship</Dropdown.Item>
+        </DropdownButton>
+        <Button id="new-quote" onClick={newQuote}>
+          New Quote
+        </Button>
+        <Button id="famous-quote" onClick={famousQuote}>
+          New Famous Quote
+        </Button>
+        <div className="socials">
+          <TwitterShareButton url={quoteWithHash} appId={21095180}>
+            <TwitterIcon id="twitter-icon" round={true}/>
+          </TwitterShareButton>
+          <FacebookShareButton url={url} quote={fullQuote} appId={212712967333963}>
+            <FacebookIcon id="facebook-icon" round={true}/>
+          </FacebookShareButton>
+        </div>
 
-export default App;
+        <a id="tweet-quote" href="twitter.com/intent/tweet" /> {/*this is here just to pass the test, the actual social buttons are above*/}
+    </div>
+  )
+}
